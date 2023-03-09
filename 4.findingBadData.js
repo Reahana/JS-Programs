899ca07351eba5643f59fa7d6d8d1896c94af611
Reahana <reahana233@gmail.com>
2023-03-09 20:06:43 +0600

    finding bad data

---
+++ 4.findingBadData.js
@@ -0,0 +1,17 @@
+ function findingBadData(numbers){
+    if(Array.isArray(numbers)){
+        let badData = 0;
+        for(let i=0; i<numbers.length;i++){
+            if(numbers[i]<0){
+                badData++;     
+            }
+        }
+        return badData;
+    }
+    return 'Error: Input should be an array';
+    
+ }
+
+
+let output = findingBadData([1,-2,0,]);
+console.log(output);
